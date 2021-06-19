e9c1b8fe7a68269ab9e7f7eb40fd04fdb37138cc
Andrew Zito <zitoa@lafayette.edu>
2021-06-19 12:27:16 -0400

    add some migrations back

---
+++ db/migrate/20210617054428_create_join_table_taggings.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableTaggings < ActiveRecord::Migration[6.1]
+  def change
+    create_join_table :snippets, :tags do |t|
+      # t.index [:snippet_id, :tag_id]
+      # t.index [:tag_id, :snippet_id]
+    end
+  end
+end
