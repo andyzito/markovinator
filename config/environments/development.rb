32fae80929ddb8468c83ddccea4620c07c4a8902
Andrew Zito <zitoa@lafayette.edu>
2021-07-16 20:20:27 -0400

    Add allowed host?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "andycodesthings.com"
+  config.hosts << "markovinator.andycodesthings.com"
 end
